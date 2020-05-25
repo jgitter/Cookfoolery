b49f97328829e6997eaaaa3296d93fab144b695a
Jeff Gitter <gitter1226@gmail.com>
2020-05-25 17:09:05 -0500

    Spring Initializr Commit for API

---
+++ cookfoolery-api/src/main/java/org/gitter/cookfoolery/CookfooleryApiApplication.java
@@ -0,0 +1,13 @@
+package org.gitter.cookfoolery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookfooleryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookfooleryApiApplication.class, args);
+	}
+
+}
---
+++ cookfoolery-api/src/test/java/org/gitter/cookfoolery/CookfooleryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gitter.cookfoolery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookfooleryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
